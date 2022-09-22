8c83d8c1cfe5c7ee4e3910658a80db6f97d71c6c
leeze <leeze0216@163.com>
2022-09-23 00:54:05 +0800

    Site updated: 2022-09-23 00:54:03

--- zone/js/friends.js
+++ zone/js/friends.js
@@ -2,23 +2,21 @@ $(document).ready(function(){
 
     let friends = {
         init: function(url) {
-            let that = this;
             $.ajaxSettings.async = false;
+            let index = layer.load(2, {shade: [0.6, '#393D49']});
             $.getJSON(url,function(result){
-                let li = "";
-                $.each(result, function(i, field){
-                    li += '<div class="card" onclick="window.open(\'' + field.url +'\')" >' +
-                        '<img className="lazyload" data-src="/images/loading.gif" src=\"'+field.img+'\" data-loaded="true">' +
-                        '<div class="card-header">' + '<div style="color: #33aaff;font-weight: bold;">' + field.name + '</div>' + '</div>' + '<div class="card-content">' + '<div style="border-top: 1px dashed #ddd;">' + field.dec + '</div>' + '</div>' + '</div>';
 
+                $.each(result, function(i, field){
+                    $(".tools").append('<div class="card" onclick="window.open(\'' + field.url +'\')" >' +
+                        '<img className="lazyload" data-src="/images/loading.gif" src=\"'+field.img+'\" data-loaded="true">' +
+                        '<div class="card-header">' + '<div style="color: #33aaff;font-weight: bold;">' + field.name + '</div>' + '</div>' + '<div class="card-content">' + '<div style="border-top: 1px dashed #ddd;">' + field.dec + '</div>' + '</div>' + '</div>');
 
                 });
-                $(".tools").append(li);
+                layer.close(index);
             });
             $.ajaxSettings.async = true;
         }
     };
-    // 传入json文件的路径
     friends.init("/zone/db/friends.json");
 
 });
